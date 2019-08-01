8da0a27cc9375a92db3f48c5745877c77601ee62
uuboy <uu_boy@foxmail.com>
2019-08-01 13:02:38 +0000

    add weappAuth

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Auth;
 use App\Http\Requests\Api\WeappAuthorizationRequest;
 
 class AuthorizationsController extends Controller
@@ -59,4 +61,26 @@ class AuthorizationsController extends Controller
 
         return $this->respondWithToken($token)->setStatusCode(201);
     }
+
+    protected function respondWithToken($token)
+    {
+    return $this->response->array([
+        'access_token' => $token,
+        'token_type' => 'Bearer',
+        'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+        ]);
+    }
+
+    public function update()
+    {
+        $token = Auth::guard('api')->refresh();
+        return $this->respondWithToken($token);
+    }
+
+    public function destroy()
+    {
+        Auth::guard('api')->logout();
+        return $this->response->noContent();
+    }
+
 }
